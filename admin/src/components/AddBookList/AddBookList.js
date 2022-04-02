3424b8feb5e1c91c033fd9fd03fa2fba2d257eb1
BIbek Neupane <nbibek17@gmail.com>
2022-04-02 18:17:05 +0545

    Changes

--- admin/src/components/AddBookList/AddBookList.js
+++ admin/src/components/AddBookList/AddBookList.js
@@ -138,136 +138,139 @@ function AddBookList(props) {
   return (
     <form className="add-book">
       <h2 className="add-book__header"> Add Book</h2>
-      <div className="add-book__book-name">
-        <label>Book Name: </label>
-        <input
-          placeholder="Book Name"
-          type="text"
-          className="txt"
-          value={formData.name}
-          onInput={(e) => setFormData({ ...formData, name: e.target.value })}
-        />
-      </div>
-      <div className="add-book__year">
-        <label>Year: </label>
-        <input
-          placeholder="Published Year"
-          type="number"
-          value={formData.year}
-          onInput={(e) => setFormData({ ...formData, year: e.target.value })}
-        />
-      </div>
-      <div className="add-book__pages">
-        <label>Pages: </label>
-        <input
-          placeholder="Book Pages"
-          type="number"
-          value={formData.pages}
-          onInput={(e) => setFormData({ ...formData, pages: e.target.value })}
-        />
-      </div>
-      <div className="add-book__isbn">
-        <label>ISBN: </label>
-        <input
-          placeholder="ISBN Number"
-          type="number"
-          value={formData.isbn}
-          onInput={(e) => setFormData({ ...formData, isbn: e.target.value })}
-        />
-      </div>
-      <div className="add-book__edition">
-        <label>Edition: </label>
-        <input
-          placeholder="Edition"
-          type="number"
-          value={formData.edition}
-          onInput={(e) => setFormData({ ...formData, edition: e.target.value })}
-        />
-      </div>
-      {!props?.book ? (
-        <div className="add-book__author">
-          <label> Authors: </label>
-          <select name="author-list" onChange={authorOnSelectChange}>
+      <div className="form-container">
+        <div className="add-book__book-name">
+          <label>Book Name: </label>
+          <input
+            placeholder="Book Name"
+            type="text"
+            className="txt"
+            value={formData.name}
+            onInput={(e) => setFormData({ ...formData, name: e.target.value })}
+          />
+        </div>
+        <div className="add-book__year">
+          <label>Year: </label>
+          <input
+            placeholder="Published Year"
+            type="number"
+            value={formData.year}
+            onInput={(e) => setFormData({ ...formData, year: e.target.value })}
+          />
+        </div>
+        <div className="add-book__pages">
+          <label>Pages: </label>
+          <input
+            placeholder="Book Pages"
+            type="number"
+            value={formData.pages}
+            onInput={(e) => setFormData({ ...formData, pages: e.target.value })}
+          />
+        </div>
+        <div className="add-book__isbn">
+          <label>ISBN: </label>
+          <input
+            placeholder="ISBN Number"
+            type="number"
+            value={formData.isbn}
+            onInput={(e) => setFormData({ ...formData, isbn: e.target.value })}
+          />
+        </div>
+        <div className="add-book__edition">
+          <label>Edition: </label>
+          <input
+            placeholder="Edition"
+            type="number"
+            value={formData.edition}
+            onInput={(e) =>
+              setFormData({ ...formData, edition: e.target.value })
+            }
+          />
+        </div>
+        {!props?.book ? (
+          <div className="add-book__author">
+            <label> Authors: </label>
+            <select name="author-list" onChange={authorOnSelectChange}>
+              <option></option>
+              {data.map((author, i) => (
+                <option key={i} data-name={author.name} data-id={author.id}>
+                  {author.name}
+                </option>
+              ))}
+            </select>
+            <button type="button" onClick={handle}>
+              Add Author
+            </button>
+            <div className="add-book__selected-authors">
+              {formData.bookAuthors.map((author, i) => (
+                <label key={i}>{author.name} </label>
+              ))}
+            </div>
+          </div>
+        ) : null}
+        <div className="add-book__publisher">
+          <label>Publisher: </label>
+          <select name="publisher-list" onChange={publisherOnSelectChange}>
             <option></option>
-            {data.map((author, i) => (
-              <option key={i} data-name={author.name} data-id={author.id}>
-                {author.name}
+            {publishers.map((publisher, i) => (
+              <option key={i} data-name={publisher.name} data-id={publisher.id}>
+                {publisher.name}
               </option>
             ))}
           </select>
-          <button type="button" onClick={handle}>
-            Add Author
-          </button>
-          <div className="add-book__selected-authors">
-            {formData.bookAuthors.map((author, i) => (
-              <label key={i}>{author.name} </label>
-            ))}
-          </div>
         </div>
-      ) : null}
-
-      <div className="add-book__publisher">
-        <label>Publisher: </label>
-        <select name="publisher-list" onChange={publisherOnSelectChange}>
-          <option></option>
-          {publishers.map((publisher, i) => (
-            <option key={i} data-name={publisher.name} data-id={publisher.id}>
-              {publisher.name}
-            </option>
-          ))}
-        </select>
+        <div className="add-book__book-types">
+          <label>Book Type: </label>
+          <select name="book-type-list" onChange={bookTypeOnSelectChange}>
+            <option></option>
+            {bookTypes.map((bookType, i) => (
+              <option key={i} data-name={bookType.name} data-id={bookType.id}>
+                {bookType.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="add-book__book-price">
+          <label>Book Price: </label>
+          <input
+            placeholder="Book Price"
+            type="number"
+            className="txt"
+            value={formData.price}
+            onInput={(e) => setFormData({ ...formData, price: e.target.value })}
+          />
+        </div>
+        <div className="add-book__description">
+          <label htmlFor="description">Description:</label>
+          <textarea
+            className="add-book__description"
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+            placeholder="Add Description"
+            value={formData.description}
+            onInput={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+          />
+        </div>
+        <div className="add-book__choose-book">
+          <label>Book PDF: </label>
+          <input placeholder="Add Description" type="file" accept=".pdf" />
+        </div>
+        <div className="add-book__book-cover">
+          <label>Book Cover: </label>
+          <input placeholder="Add Description" type="file" accept="image/*" />
+        </div>
+        <button
+          className="add-book__btn-add"
+          onClick={handleBookAdd}
+          type="submit"
+        >
+          Add
+        </button>
       </div>
-      <div className="add-book__book-types">
-        <label>Book Type: </label>
-        <select name="book-type-list" onChange={bookTypeOnSelectChange}>
-          <option></option>
-          {bookTypes.map((bookType, i) => (
-            <option key={i} data-name={bookType.name} data-id={bookType.id}>
-              {bookType.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="add-book__book-price">
-        <label>Book Price: </label>
-        <input
-          placeholder="Book Price"
-          type="number"
-          className="txt"
-          value={formData.price}
-          onInput={(e) => setFormData({ ...formData, price: e.target.value })}
-        />
-      </div>
-      <div className="add-book__description">
-        <label htmlFor="description">Description:</label>
-        <textarea
-          className="add-book__description"
-          name="description"
-          id="description"
-          cols="30"
-          rows="10"
-          placeholder="Add Description"
-          value={formData.description}
-          onInput={(e) =>
-            setFormData({ ...formData, description: e.target.value })
-          }
-        />
-      </div>
-      <div className="add-book__choose-book">
-        <label>Book PDF: </label>
-        <input placeholder="Add Description" type="file" accept=".pdf" />
-      </div>
-      <div className="add-book__book-cover">
-        <label>Book Cover: </label>
-        <input placeholder="Add Description" type="file" accept="image/*" />
-      </div>
-      <button
-        className="add-book__btn-add"
-        onClick={handleBookAdd}
-        type="submit"
-      >
-        Add
-      </button>
     </form>
   );
 }
--- admin/src/components/BookList/BookList.js
+++ admin/src/components/BookList/BookList.js
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
 import AddBookList from "../AddBookList/AddBookList";
 import "./BookList.scss";
 
 function BookAuthorList(props) {
-  const [data, setData] = useState([]);
-  useEffect((_) => {
-    fetch(`http://localhost:3000/bookauthors/${props.bookId}`)
-      .then((response) => response.json())
-      .then((json) => setData(json.authors));
-  }, []);
+  const data =
+    useFetch(`http://localhost:3000/bookauthors/${props.bookId}`)?.authors ||
+    [];
+
   return data.map((author, i) => <span key={i}>{author.name}</span>);
+}
+
+function Publisher(props) {
+  const publisher =
+    useFetch(`http://localhost:3000/publishers/${props.id}`)?.publisher || [];
+
+  return (
+    <>
+      {publisher.map((pub, i) => (
+        <span key={i}>{pub.name}</span>
+      ))}
+    </>
+  );
 }
 
 async function deleteBtn(id) {
@@ -55,15 +67,16 @@ function BookList(props) {
               <div className="authors">
                 <BookAuthorList bookId={book.id} />
               </div>
+              <b>
+                <Publisher id={book.publisherId} />
+              </b>
               <div className="price">RS. {book.price}</div>
             </div>
             <div className="right">
               <button
                 className="btn"
                 title="Edit"
-                onClick={(_) =>
-                  props.onShowPopUp(<AddBookList book={book} />)
-                }
+                onClick={(_) => props.onShowPopUp(<AddBookList book={book} />)}
               >
                 Edit
               </button>
