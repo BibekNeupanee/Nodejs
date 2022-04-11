00c3ce960f85411c750373b2b8e1bd28d99a2397
BIbek Neupane <nbibek17@gmail.com>
2022-04-11 23:03:34 +0545

    header

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -20,70 +20,24 @@ function Header() {
       <header class="main-header">
         <div class="main-header__top">
           <div class="main-header__contact">
-            <i class="fa-solid fa-mobile-button"></i> 984734843
+            <i class="fa-solid fa-mobile-button" />
+            +977-9861282149
           </div>
           <div class="main-header__user-links">
-            <a href="#">
-              <i class="fa-solid fa-user"></i>
-            </a>
-            <a href="#">
+            <Link to={"/profile/" + user?.username} href="#">
+              <i class="fa-solid fa-user" />
+              <span>{user?.name}</span>
+            </Link>
+            <Link to={"/cart"} href="#">
               <i class="fa-solid fa-cart-shopping"></i>
-            </a>
-            <a href="#">
+            </Link>
+            <Link to={"/"} onClick={() => handleLogout()}>
               <i class="fa-solid fa-right-from-bracket"></i>
-            </a>
+            </Link>
           </div>
         </div>
-        <div class="main-header__bottom">
-          <div class="logo">Book</div>
-          <nav>
-            <ul>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Categories</a>
-              </li>
-              <li>
-                <a href="#">Authors</a>
-              </li>
-              <li>
-                <a href="#">Publishers</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <div class="main-search">
-            <input type="text" placeholder="Search for Books by Keyword" />
-            <button>
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </div>
-        </div>
+        <BottomHeader />
       </header>
-      // <header class="main-header">
-      //   <div class="main-header__top">
-      //     <div class="main-header__contact">
-      //       <i class="fa-solid fa-mobile-button" />
-      //       +977-9861282149
-      //     </div>
-      //     <div class="main-header__user-links">
-      //       <Link to={"/profile/" + user?.username} href="#">
-      //         <i class="fa-solid fa-user" />
-      //         <span>{user?.name}</span>
-      //       </Link>
-      //       <Link to={"/cart"} href="#">
-      //         <i class="fa-solid fa-cart-shopping"></i>
-      //       </Link>
-      //       <Link to={"/"} onClick={() => handleLogout()}>
-      //         <i class="fa-solid fa-right-from-bracket"></i>
-      //       </Link>
-      //     </div>
-      //   </div>
-      //   <BottomHeader />
-      // </header>
     );
   } else {
     return (
@@ -124,15 +78,6 @@ function Header() {
       // </div>
     );
   }
-
-  // <header>
-  //   <Link to={"/"} className="logo">
-  //     <img src={logo} alt="abc" className="image" />
-  //   </Link>
-  //   <Menu />
-  //   <SearchBox />
-  //   <Settings />
-  // </header>
 }
 
 export default Header;
