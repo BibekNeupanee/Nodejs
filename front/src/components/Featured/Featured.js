d663106a79a11f820c304c905ce728e2cdf40ca6
BIbek Neupane <nbibek17@gmail.com>
2022-04-11 22:55:33 +0545

    featured

--- front/src/components/Featured/Featured.js
+++ front/src/components/Featured/Featured.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
+import BookAuthorList from "../BookAuthorList/BookAuthorList";
 
 function Featured() {
+  const books = useFetch("http://localhost:3000/books")?.books || [];
   return (
     <section class="featured">
       <header>
@@ -10,190 +13,34 @@ function Featured() {
         </a>
       </header>
       <main>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
+        {[...books].slice(0, 20).map((book, i) => (
+          <div class="book">
+            <img
+              src={
+                book.image ||
+                "https://www.mswordcoverpages.com/wp-content/uploads/2018/10/Book-cover-page-3-CRC.png"
+              }
+              alt={book.name}
+            />
+            <div class="info">
+              <a href="#" class="title">
+                {book.name}
+              </a>
+              <a href="#" class="author">
+                <BookAuthorList bookId={book.id} />
+              </a>
+              <div class="price">Rs. {book.price}</div>
+            </div>
+            <div class="controls">
+              <a href="#" class="add-to-cart">
+                Add to cart
+              </a>
+              <a href="#">
+                <i class="fa-solid fa-heart"></i>
+              </a>
+            </div>
           </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
-        <div class="book">
-          <img
-            src="https://demo2.madrasthemes.com/bookworm-html/redesigned-octo-fiesta/assets/img/150x226/img1.jpg"
-            alt=""
-          />
-          <div class="info">
-            <a href="#" class="title">
-              Think Like a Monk: Train Your Mind for Peace and Purpose Everyday
-            </a>
-            <a href="#" class="author">
-              Jay Shetty
-            </a>
-            <div class="price">$29</div>
-          </div>
-          <div class="controls">
-            <a href="#" class="add-to-cart">
-              Add to cart
-            </a>
-            <a href="#">
-              <i class="fa-solid fa-heart"></i>
-            </a>
-          </div>
-        </div>
+        ))}
       </main>
     </section>
   );
