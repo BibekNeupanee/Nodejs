bed4465ac8d07b21ae4bbb8ea70c435bf87e00b2
BIbek Neupane <nbibek17@gmail.com>
2022-04-12 16:17:52 +0545

    Login

--- front/src/components/UserLogin/UserLogin.js
+++ front/src/components/UserLogin/UserLogin.js
@@ -30,30 +30,78 @@ function UserLogin() {
   };
 
   return (
-    <form className="login">
-      <label className="user_login">User Login</label>
-      <label className="email">Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="eg@abc.com"
-      />
-      <label className="password">Password:</label>
-      <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <Link
-        className="btn-login"
-        onClick={(e) => {
-          handleLoginBtn();
-          e.preventDefault();
-          // window.location.href = "/";
-        }}
-        to={"/"}
-      >
-        Login
-      </Link>
-      <Link to={"/register"}>Create an account</Link>
-      <a href="/login">Forget Password?</a>
-    </form>
+    // <form className="login">
+    //   <label className="user_login">User Login</label>
+    //   <label className="email">Email:</label>
+    //   <input
+    //     type="email"
+    //     onChange={(e) => setEmail(e.target.value)}
+    //     placeholder="eg@abc.com"
+    //   />
+    //   <label className="password">Password:</label>
+    //   <input type="password" onChange={(e) => setPassword(e.target.value)} />
+    //   <Link
+    //     className="btn-login"
+    //     Link
+    //   >
+    //     Login
+    //   </Link>
+    //   <Link to={"/register"}>Create an account</Link>
+    //   <a href="/login">Forget Password?</a>
+    // </form>
+    <main class="main-main">
+      <section class="login">
+        <header>
+          <div class="title">
+            Login <br />
+            <small>for best experience</small>
+          </div>
+        </header>
+        <main>
+          <form method="post">
+            <div class="group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="eg@abc.com"
+              />
+            </div>
+            <div class="group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div class="group">
+              <Link
+                className="login"
+                onClick={(e) => {
+                  handleLoginBtn();
+                  e.preventDefault();
+                  // window.location.href = "/";
+                }}
+                to={"/"}
+                type="submit"
+              >
+                Login
+              </Link>
+            </div>
+
+            <div class="group">
+              <span>
+                Don't have account? <Link to={"/register"}>Sign up</Link>
+              </span>
+            </div>
+          </form>
+        </main>
+      </section>
+    </main>
   );
 }
 
